8d6995ebf0621232b40430f29b560a56fd870b6a
Johnathon <johnathondouglass@gmail.com>
2022-05-30 22:58:45 -0700

    pushing the forgotten files

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const port = process.env.PORT || 5001;
-
 module.exports = axios.create({
-  baseURL: `http://localhost:${port}/products`
+  baseURL: `http://localhost:5000/products`
 });
